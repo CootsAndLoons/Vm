9b5d60ea5ed4b3eb80d1db9f0bee6301787757fd
kriskata06 <kbs_teen16@abv.bg>
2025-04-10 17:22:54 +0300

    Teams stuff

---
+++ VacationManager/Vm/Controllers/TeamsController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Vm.Data;
+using Vm.Models;
+
+namespace Vm.Controllers
+{
+    public class TeamsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TeamsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var teams = await _context.Teams.Include(t => t.Project).Include(t => t.TeamLead).ToListAsync();
+            return View(teams);
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["Projects"] = new SelectList(_context.Projects, "Id", "Name");
+            ViewData["TeamLeads"] = new SelectList(
+                _context.Users.Where(u => u.Role.Name == "Team Lead"),
+                "Id",
+                "UserName" // Или FirstName + LastName
+            );
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Team team)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(team);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(team);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var team = await _context.Teams.FindAsync(id);
+            if (team == null) return NotFound();
+
+            ViewData["Projects"] = new SelectList(_context.Projects, "Id", "Name", team.ProjectId);
+            ViewData["TeamLeads"] = new SelectList(
+                _context.Users.Where(u => u.Role != null && u.Role.Name == "Team Lead"),
+                "Id",
+                "UserName",
+                team.TeamLeadId);
+
+            return View(team);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Team team)
+        {
+            if (id != team.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(team);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(team);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var team = await _context.Teams.FindAsync(id);
+            if (team == null) return NotFound();
+            return View(team);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var team = await _context.Teams.FindAsync(id);
+            if (team != null)
+            {
+                _context.Teams.Remove(team);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
